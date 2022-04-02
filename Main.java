b607839b44c829bb6154b95d68a1de0b8eab72ed
Horhe305 <101187054+Horhe305@users.noreply.github.com>
2022-04-02 17:41:52 +0000

    koniec

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+import java.util.Scanner; 
+class Main
+{
+  public static void main(String[] args)
+  {
+    Scanner Obj = new Scanner(System.in); 
+    double f, c, d;
+
+    d = 0.56;
+    System.out.println("F C");
+    for(f =- 450; f <= 136; f++)
+    {
+      c = d*(f - 32);
+      System.out.println(f + " " + c);
+    }
   }
 }
